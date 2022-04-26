1c108314b17f91563272372c97fb38f37ce9612d
takajinomotoazt <70266762+takajinomotoazt@users.noreply.github.com>
2022-04-26 23:55:47 +1200

    dice 1
    Signed-off-by: takajinomotoazt <‘70266762+takajinomotoazt@users.noreply.github.com’>

--- ITP1/ITP1_11_A.cpp
+++ ITP1/ITP1_11_A.cpp
@@ -1,80 +1,110 @@
 #include <iostream>
-#include <string>
-#include <iomanip>
-#include <cmath>
 
 using namespace std;
 
-double avg(double data[], int n)
+typedef struct
 {
-  int i;
-  double sum = 0.0;
-  for (i = 0; i < n; i++)
-    sum += data[i];
-  return sum / n;
-}
-double dev(double data[], int n)
+  int T1;
+  int B6;
+  int E3;
+  int W4;
+  int S2;
+  int N5;
+} dice_t;
+
+void rotate(dice_t *p, char direction)
 {
-  int i;
-  double m = avg(data, n);
-  double var = 0.0;
-  for (i = 0; i < n; i++)
-    var += (data[i] - m) * (data[i] - m);
-  return sqrt(var / n);
+  int w;
+
+  switch (direction)
+  {
+  case 'E':
+    // 1 -> W
+    w = p->T1;
+    // 4 -> 1
+    p->T1 = p->W4;
+    // 6 -> 4
+    p->W4 = p->B6;
+    // 3 -> 6
+    p->B6 = p->E3;
+    // 1 -> 3
+    p->E3 = w;
+    /* code */
+    break;
+  case 'W':
+
+    // 1 w
+    w = p->T1;
+    // 3 1
+    p->T1 = p->E3;
+    // 6 3
+    p->E3 = p->B6;
+    // 4 6
+    p->B6 = p->W4;
+    // w 4
+    p->W4 = w;
+    break;
+  case 'S':
+
+    // 1 -> W
+    w = p->T1;
+    // 5 1
+    p->T1 = p->N5;
+    // 6 5
+    p->N5 = p->B6;
+    // 2 6
+    p->B6 = p->S2;
+    // 1 2
+    p->S2 = w;
+    break;
+  case 'N':
+
+    // 1 -> W
+    w = p->T1;
+    // 2 1
+    p->T1 = p->S2;
+    // 6 2
+    p->S2 = p->B6;
+    // 5 6
+    p->B6 = p->N5;
+    // 1 5
+    p->N5 = w;
+    break;
+
+  default:
+    break;
+  }
 }
 
 int main()
 {
-  int n[100];
-  int s[100][1000];
 
-  int cntN = 0;
-  int cntS;
+  dice_t dice;
+  string cmd;
 
-  do
+  cin >> dice.T1 >> dice.S2 >> dice.E3 >> dice.W4 >> dice.N5 >> dice.B6;
+
+  cin >> cmd;
+
+  // // print all
+  // cout << "dice.T1=" << dice.T1 << " ";
+  // cout << "dice.S2=" << dice.S2 << " ";
+  // cout << "dice.E3=" << dice.E3 << " ";
+  // cout << "dice.W4=" << dice.W4 << " ";
+  // cout << "dice.N5=" << dice.N5 << " ";
+  // cout << "dice.B6=" << dice.B6 << endl;
+  // cout << "cmd=" << cmd << endl;
+
+  // process commands
+  char c;
+  for (int i = 0; i < cmd.length(); i++)
   {
-    //
-    cin >> n[cntN];
-    if (n[cntN] == 0)
-    {
-      break;
-    }
-
-    for (int i = 0; i < n[cntN]; i++)
-    {
-      cin >> s[cntN][i];
-    }
-
-    cntN++;
-  } while (true);
-
-  // // print all data
-  // for (int i = 0; i < cntN; i++)
-  // {
-  //   cout << "s[" << i << "]";
-  //   for (int j = 0; j < n[i]; j++)
-  //   {
-  //     // cout << " [" << j << "]=" << s[i][j];
-  //     cout << " " << s[i][j];
-  //   }
-  //   cout << endl;
-  // }
-
-  // cout << "#####" << endl;
-
-  // compute
-  for (int i = 0; i < cntN; i++)
-  {
-    double avg, dev, sum = 0.0, sum2 = 0.0;
-    for (int j = 0; j < n[i]; j++)
-    {
-      sum += s[i][j];
-      sum2 += s[i][j] * s[i][j];
-    }
-    avg = sum / n[i];
-    dev = sqrt(sum2 / n[i] - avg * avg);
-    cout << fixed << setprecision(8) << dev << endl;
+    /* code */
+    c = cmd.at(i);
+    rotate(&dice, c);
   }
+
+  cout << dice.T1 << endl;
 
   return 0;
 }
